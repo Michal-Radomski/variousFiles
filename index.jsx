002d4778bfa7d14b4a07b883e67eb2f7d0b7d469
Michal Radomski <michalra@op.pl>
2021-09-03 18:19:13 +0200

    one line react app

---
+++ index.jsx
@@ -0,0 +1,13 @@
+// +One file React App
+
+// Import the React and ReactDom libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a React Component
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+// Take the React Component and show it on the show
+ReactDOM.render(<App />, document.getElementById("root"));
