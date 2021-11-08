aa3c11c5d6e38872315d4867941992807c50a7c5
Michal Radomski <michalra@op.pl>
2021-11-08 21:29:15 +0100

    bind(), call(), apply() - done

---
+++ 01 async_JS + this + extras/bind_apply_call.js
@@ -0,0 +1,50 @@
+//- Tutorial: https://www.youtube.com/watch?v=TQXZL4bvw9Q
+
+const human = {
+  name: "Michal",
+  sayName() {
+    console.log(`My name is ${this.name}`);
+  },
+  sayFullName: function (surname) {
+    console.log(`My name is ${this.name} ${this.surname ? this.surname : surname}`);
+  },
+  sayFullName2: function (surname) {
+    console.log(`My name is ${this.name} ${this.surname ? this.surname : surname}`);
+  },
+  //- To samo co wyżej
+  sayName2: function () {
+    console.log(`My name is ${this.name}`);
+  },
+};
+human.sayName();
+human.sayName2();
+
+//- call()
+const personA = {
+  name: "John",
+};
+const personB = {
+  name: "Donald",
+};
+human.sayName.call(personA); //My name is John -> kontekst jest zmieniony
+
+const people = [
+  {
+    name: "John",
+    surname: "King",
+  },
+  {
+    name: "Donald",
+  },
+];
+people.forEach((p) => human.sayName.call(p));
+
+//- apply()
+people.forEach((p) => human.sayFullName.apply(p, ["Kowalski"])); //My name is John King, My name is Donald Kowalski
+
+//- bind() // To samo bo apply - funkcja nie wykona się od razu tylko w momencie uruchomienia!
+const people2 = people;
+people2.forEach((p) => {
+  const peopleBind = human.sayFullName2.bind(p, "Kowalski"); //My name is John King, My name is Donald Kowalski - ale dopiero w momencie uruchomienia!
+  peopleBind();
+});
