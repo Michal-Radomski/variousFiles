c1da52f8d6732eb6dce4c1700dcd719c6da7d964
Michal Radomski <michalra@op.pl>
2022-05-02 16:22:08 +0200

    ReadLine was added

--- 05 Node18/index.ts
+++ 05 Node18/index.ts
@@ -1,5 +1,7 @@
-// Fetching with Node18
+const readline = require("readline");
+const {stdin: input, stdout: output} = require("process");
 
+// Fetching with Node18
 const testingFetchWithNode18 = async function () {
   const response: Response = await fetch("https://ipwhois.app/json/?objects=ip,country,city");
   if (response.ok) {
@@ -9,3 +11,17 @@ const testingFetchWithNode18 = async function () {
 };
 
 testingFetchWithNode18();
+
+// Readline with Node18
+const nodeReadLine = async function () {
+  const rl = readline.createInterface(process.stdin, process.stdout);
+
+  rl.setPrompt(`What is your name? `);
+  rl.prompt();
+  rl.on("line", (name: string) => {
+    console.log(`Your name is: ${name}`);
+    rl.close();
+  });
+};
+
+setTimeout(nodeReadLine, 1500);
