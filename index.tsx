d6e1a01fa60f452b48a6d6dff168abb041656d35
Michal Radomski <michalra@op.pl>
2022-05-07 01:10:29 +0200

    index.jsx -> index.tsx

--- index.tsx
+++ index.tsx
@@ -1,17 +1,23 @@
-// +One file React App
+// +One file React App in TS
 
 // Import the React and ReactDom libraries
 import React from "react";
 import ReactDOM from "react-dom";
 
+//* Install: npm install --save-dev @types/webpack-env
 if (module.hot) {
   module.hot.accept();
 }
 
 // Create a React Component
-const App = () => {
+const App = (): JSX.Element => {
   return <div>Hi there!</div>;
 };
 
 // Take the React Component and show it on the show
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root") as HTMLElement
+);
