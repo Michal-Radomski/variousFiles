a83680258fe95cfa03e26de4c20145b4d1f303d6
Michal Radomski <michalra@op.pl>
2022-05-07 02:02:24 +0200

    Another example was added

--- 06 CascadeDesignPattern/index.ts
+++ 06 CascadeDesignPattern/index.ts
@@ -4,6 +4,8 @@ let string: string = "string cheese";
 let string2: string = string.split("").reverse().join();
 console.log("string2:", string2);
 
+//* ---
+
 function number(this: any, value: number): void {
   this.value = value;
   this.plus = function (sum) {
@@ -22,6 +24,8 @@ number.prototype.minus = function (min) {
 };
 console.log("new number(5).plus(1).minus(2).return():", new number(5).plus(1).minus(2).return()); // 4
 
+//* ---
+
 function init(number: number) {
   let _Key: number = Math.random() * 100;
   let n = {
@@ -37,3 +41,50 @@ function init(number: number) {
   return n;
 }
 console.log("init(1).plus(1).plus(2).return():", init(1).plus(1).plus(2).return()); // 4
+
+// * ---
+
+{
+  let Calc = function (this: any): void {
+    this.result = 0 as number;
+    console.log("this:", this);
+
+    this.add = function () {
+      for (let x = 0; x < arguments.length; x++) {
+        this.result += arguments[x];
+      }
+      return this;
+    };
+
+    this.sub = function () {
+      for (let x = 0; x < arguments.length; x++) {
+        this.result -= arguments[x];
+      }
+      return this;
+    };
+
+    this.multiply = function () {
+      if (this.result == 0) this.result = 1;
+      for (let x = 0; x < arguments.length; x++) {
+        this.result *= arguments[x];
+      }
+      return this;
+    };
+
+    this.div = function () {
+      if (this.result == 0) return this;
+      for (let x = 0; x < arguments.length; x++) {
+        this.result /= arguments[x];
+      }
+      return this;
+    };
+
+    this.eq = function () {
+      return this.result;
+    };
+  };
+
+  let cal1 = new Calc();
+
+  console.log("cal1.add(3).sub(1).add(2):", cal1.add(3).sub(1).add(2)); // Here result = 4;
+}
