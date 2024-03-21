660873fb23ff18e07f1d4a4e676670e7c1756d61
Michal Radomski <michalra@op.pl>
2024-03-21 17:54:36 +0100

    Litte update

--- 08 Destructuring Assignment/index.ts
+++ 08 Destructuring Assignment/index.ts
@@ -1,25 +1,46 @@
 interface Test {
-  a: {b: {c: {d: number}}};
+  a: { b: { c: { d: number } } };
 }
 
 const test: Test = {
   a: {
-    b: {c: {d: 4}},
+    b: { c: { d: 4 } },
   },
 };
 
 const test2: Object = test.a.b.c;
 
-console.log({test2});
+console.log({ test2 });
 
 const test3: Object = test["a"]["b"]["c"];
 
-console.log({test3});
+console.log({ test3 });
 
 const {
   a: {
-    b: {c},
+    b: { c },
   },
 } = test;
 
-console.log({c});
+console.log({ c });
+
+//* Destructive assignment with optional chaining
+const obj = {
+  foo: {
+    bar: {
+      baz: "value",
+    },
+  },
+};
+
+// Without optional chaining and destructuring
+const value1 = obj.foo && obj.foo.bar && obj.foo.bar.baz;
+console.log(value1); // Outputs: 'value'
+
+// With optional chaining and destructuring
+const {
+  foo: {
+    bar: { baz },
+  },
+} = obj ?? {};
+console.log(baz); // Outputs: 'value
