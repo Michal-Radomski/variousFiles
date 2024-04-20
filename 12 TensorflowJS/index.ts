09835af6ae224b914d8d6268bed64bf60745e0dc
Michal Radomski <michalra@op.pl>
2024-04-20 22:52:03 +0200

    Tensorflow.js exercise - commit 02

--- 12 TensorflowJS/index.ts
+++ 12 TensorflowJS/index.ts
@@ -4,21 +4,48 @@ import * as tf from "@tensorflow/tfjs-node";
 // console.log("tf.getBackend():", tf.getBackend());
 // console.log("tf.memory():", tf.memory());
 
-// Create a simple model.
-const model: tf.Sequential = tf.sequential();
-model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+//* Create a simple model.
+// const model: tf.Sequential = tf.sequential();
+// model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 
-// Prepare the model for training: Specify the loss and the optimizer.
-model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
+// // Prepare the model for training: Specify the loss and the optimizer.
+// model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
 
-// Generate some synthetic data for training. (y = 2x - 1)
-const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+// // Generate some synthetic data for training. (y = 2x - 1)
+// const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+// const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
 
-// Train the model using the data.
-(async function () {
-  await model.fit(xs, ys, { epochs: 1000 });
-  const result = (await model.predict(tf.tensor2d([20], [1, 1]))) as tf.Tensor<tf.Rank>;
-  result.print(); //* Tensor<Rank>
-  model.summary();
-})();
+// // Train the model using the data.
+// (async function () {
+//   await model.fit(xs, ys, { epochs: 1000 });
+//   const result = (await model.predict(tf.tensor2d([20], [1, 1]))) as tf.Tensor<tf.Rank>;
+//   result.print(); //* Tensor<Rank>
+//   model.summary();
+// })();
+
+//* Linear Regression
+
+interface Car {
+  mpg?: number;
+  Miles_per_Gallon?: number;
+  Horsepower?: number;
+  horsepower?: number;
+}
+
+async function getData(): Promise<Car[]> {
+  const carsDataResponse = await fetch("https://storage.googleapis.com/tfjs-tutorials/carsData.json");
+  const carsData = (await carsDataResponse.json()) as Car[];
+  // console.log("carsData:", carsData);
+
+  const cleaned = carsData
+    .map((car: Car) => ({
+      mpg: car.Miles_per_Gallon,
+      horsepower: car.Horsepower,
+    }))
+    .filter((car: Car) => car.mpg !== null && car.horsepower !== null);
+
+  console.log("cleaned:", cleaned);
+  return cleaned;
+}
+
+// getData();
