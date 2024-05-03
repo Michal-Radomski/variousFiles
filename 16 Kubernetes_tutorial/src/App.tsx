4c8e38a03a09fbd0c3df56918c874b1289be3914
Michal Radomski <michalra@op.pl>
2024-05-03 22:35:55 +0200

    React + Kubernetes tutorial - initial commit

---
+++ 16 Kubernetes_tutorial/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.scss";
+
+const App = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <h1 className="text-center text-muted py-5">React + Kubernetes tutorial</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
