0e8ebf72c8bfe56bd2ef7a65eee5c03b879d44d4
Michal Radomski <michalra@op.pl>
2024-05-12 18:15:43 +0200

    React-electron exercise

---
+++ 19_React_Electron/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 className="text-center text-muted mt-5">React Electron App</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
