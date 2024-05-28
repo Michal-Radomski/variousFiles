7d129e10fd35d7bd14ab3aee5b9a72cb40af7288
Michal Radomski <michalra@op.pl>
2024-05-29 00:29:07 +0200

    simpleAlgorithmCSPRNG.ts was added

--- 21_Gists/no_gist/simpleAlgorithmCSPRNG.ts
+++ 21_Gists/no_gist/simpleAlgorithmCSPRNG.ts
@@ -1,37 +1,43 @@
+//* CSPRNG -> Cryptographically secure pseudorandom number generator (with a little help from OpenAI_3.5)
+import * as crypto from "crypto";
+
 class SimpleCSPRNG {
-  crypto: Crypto;
   state: Uint8Array;
+  crypto: typeof crypto;
   constructor() {
     // Use crypto module if available
-    if (typeof window !== "undefined" && window.crypto) {
-      this.crypto = window.crypto;
-    } else {
-      this.crypto = require("crypto");
-    }
+    // if (typeof window !== "undefined" && window.crypto) {
+    //   this.crypto = window.crypto;
+    // } else {
+    //   this.crypto = require("crypto");
+    // }
+    this.crypto = crypto;
 
     // Initialize the state with a random seed
     this.state = this.getRandomBytes(16);
-    console.log("this.state:", this.state);
+    // console.log("this.state:", this.state);
   }
 
   // Method to get random bytes
   getRandomBytes(size: number) {
     const buffer = new Uint8Array(size);
+    // console.log("buffer:", buffer);
     if (this.crypto.getRandomValues) {
       this.crypto.getRandomValues(buffer);
     } else {
-      // @ts-ignore
       const randomBytes = this.crypto.randomBytes(size);
       for (let i = 0; i < size; i++) {
         buffer[i] = randomBytes[i];
       }
     }
+    // console.log("buffer:", buffer);
     return buffer;
   }
 
   // Method to mix the current state with new entropy
   mixState() {
     const newEntropy = this.getRandomBytes(16);
+    // console.log("newEntropy:", newEntropy);
     for (let i = 0; i < this.state.length; i++) {
       this.state[i] ^= newEntropy[i];
     }
@@ -43,6 +49,7 @@ class SimpleCSPRNG {
     for (let i = 0; i < data.length; i++) {
       hash = (hash * 31 + data[i]) & 0xffffffff;
     }
+    // console.log("hash:", hash);
     return hash;
   }
 
@@ -50,8 +57,8 @@ class SimpleCSPRNG {
   getRandomNumber() {
     this.mixState(); // Mix the state with new entropy
     const hash = this.hash(this.state);
-    console.log({ hash });
-    console.log(hash >>> 0);
+    // console.log({ hash });
+    // console.log("hash >>> 0:", hash >>> 0);
     return hash >>> 0; // Convert to unsigned 32-bit integer
   }
 
@@ -64,5 +71,6 @@ class SimpleCSPRNG {
 
 // Example usage
 const csprng = new SimpleCSPRNG();
+// console.log("csprng:", csprng);
 console.log("Random Number:", csprng.getRandomNumber());
 console.log("Random Integer between 1 and 100:", csprng.getRandomInt(1, 100));
