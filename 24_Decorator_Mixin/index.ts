dc5c1397267ada03c06e3c5218f991aa6b078520
Michal Radomski <michalra@op.pl>
2024-06-15 22:36:10 +0200

    Decorator exercise 2 added

--- 24_Decorator_Mixin/index.ts
+++ 24_Decorator_Mixin/index.ts
@@ -1,15 +1,40 @@
 //* 1. Decorator exercise
-function LogClassName(constructor: Function) {
-  console.log(`Class created: ${constructor.name}`);
+// function LogClassName(constructor: Function) {
+//   console.log(`Class created: ${constructor.name}`);
+// }
+
+// @LogClassName
+// class MyClass {
+//   constructor() {
+//     console.log("MyClass instance created");
+//   }
+// }
+
+// // When you create an instance of MyClass, the decorator will log the class name
+// const instance = new MyClass();
+// console.log("instance:", instance);
+
+//* 2. Decorator exercise
+function LogMethod(_target: any, propertyName: string, propertyDescriptor: PropertyDescriptor): PropertyDescriptor {
+  const method = propertyDescriptor.value;
+
+  propertyDescriptor.value = function (...args: any[]) {
+    console.log(`Call: ${propertyName}(${args.join(", ")})`);
+    const result = method.apply(this, args);
+    console.log(`Return: ${result}`);
+    return result;
+  };
+
+  return propertyDescriptor;
 }
 
-@LogClassName
-class MyClass {
-  constructor() {
-    console.log("MyClass instance created");
+class MyMath {
+  @LogMethod
+  add(a: number, b: number): number {
+    return a + b;
   }
 }
 
-// When you create an instance of MyClass, the decorator will log the class name
-const instance = new MyClass();
-console.log("instance:", instance);
+const math = new MyMath();
+const result = math.add(2, 3); // Logs: Call: add(2, 3) and Return: 5
+console.log("result:", result); // Outputs: 5
