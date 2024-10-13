393823479d600340b5e35ce8b07ea7e552990039
Michal Radomski <michalra@op.pl>
2024-10-13 12:21:45 +0200

    Styled-components exercise was added

--- 28_React/src/styles/StylesWrapper.tsx
+++ 28_React/src/styles/StylesWrapper.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import EmotionButton from "./StyledButton";
 import JSSButton from "./JSS_Button";
+import StyledComponentsButton from "./StyledComponentsButton";
 
 const StylesWrapper = (): JSX.Element => {
   return (
@@ -19,6 +20,13 @@ const StylesWrapper = (): JSX.Element => {
         <div>
           <JSSButton primary={true} label="Primary Button" />
           <JSSButton label="Secondary Button" />
+        </div>
+      </div>
+
+      <div>
+        <h3>Styled-Components library</h3>
+        <div>
+          <StyledComponentsButton />
         </div>
       </div>
     </React.Fragment>
