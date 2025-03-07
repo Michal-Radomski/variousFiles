2127aaddf94416c54488c6a8071c58211565297d
Michal Radomski <michalra@op.pl>
2025-03-08 00:12:31 +0100

    Exercise was added

--- 28_React/src/patterns/CompoundComponent.tsx
+++ 28_React/src/patterns/CompoundComponent.tsx
@@ -1,7 +1,59 @@
+//* A Compound Component is a design pattern where multiple components work together to provide a cohesive functionality.
 import React from "react";
 
+// Define the props for MenuItem
+interface MenuItemProps {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+// MenuItem component
+const MenuItem: React.FC<MenuItemProps> = ({ children, onClick }): JSX.Element => {
+  return <li onClick={onClick}>{children}</li>;
+};
+
+// Define the props for SubMenu
+interface SubMenuProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+// SubMenu component
+const SubMenu: React.FC<SubMenuProps> = ({ title, children }): JSX.Element => {
+  return (
+    <div>
+      <h4>{title}</h4>
+      <ul>{children}</ul>
+    </div>
+  );
+};
+
+// Menu component
+interface MenuProps {
+  children: React.ReactNode;
+}
+
+const Menu: React.FC<MenuProps> = ({ children }): JSX.Element => {
+  return (
+    <div>
+      <h3>Menu</h3>
+      <ul>{children}</ul>
+    </div>
+  );
+};
+
 const CompoundComponent = (): JSX.Element => {
-  return <React.Fragment>CompoundComponent</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Menu>
+        <MenuItem onClick={() => console.log("Home clicked")}>Home</MenuItem>
+        <SubMenu title="Settings">
+          <MenuItem onClick={() => console.log("Settings clicked")}>Settings</MenuItem>
+          <MenuItem onClick={() => console.log("Advanced clicked")}>Advanced</MenuItem>
+        </SubMenu>
+      </Menu>
+    </React.Fragment>
+  );
 };
 
 export default CompoundComponent;
